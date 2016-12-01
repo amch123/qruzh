f602e4e51834aad5e25838fae2b333243da44465
amch123 <angelicacovahuerta@gmail.com>
2016-12-01 16:31:20 -0400

    cambios

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -7,28 +7,28 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    {{ Html::style('controlpanel/bootstrap/css/bootstrap.min.css') }}
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+    {{ Html::style('controlpanel/dist/css/AdminLTE.min.css') }}
     <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+    {{ Html::style('controlpanel/dist/css/skins/_all-skins.min.css') }}
     <!-- iCheck -->
-    <link rel="stylesheet" href="plugins/iCheck/flat/blue.css">
+    {{ Html::style('controlpanel/plugins/iCheck/flat/blue.css') }}
     <!-- Morris chart -->
-    <link rel="stylesheet" href="plugins/morris/morris.css">
+    {{ Html::style('controlpanel/plugins/morris/morris.css') }}
     <!-- jvectormap -->
-    <link rel="stylesheet" href="plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+    {{ Html::style('controlpanel/plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}
     <!-- Date Picker -->
-    <link rel="stylesheet" href="plugins/datepicker/datepicker3.css">
+    {{ Html::style('controlpanel/plugins/datepicker/datepicker3.css') }}
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+    {{ Html::style('controlpanel/plugins/daterangepicker/daterangepicker.css') }}
     <!-- bootstrap wysihtml5 - text editor -->
-    <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+    {{ Html::style('controlpanel/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -317,88 +317,7 @@
 @endsection
 
 @section('footer')   
-    <footer id="footer" class="color color-primary">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="newsletter">
-                        <h4>Noticias</h4>
-                        <p>Mantente actualizado en todo lo relacionado <br> a nuestros productos.</p>
-            
-                        <div class="alert alert-success hidden" id="newsletterSuccess">
-                            <strong>Éxito!</strong> Has sido agregado a nuestra lista de correos electrónicos.
-                        </div>
-            
-                            <div class="alert alert-danger hidden" id="newsletterError"></div>
-            
-                            <form id="newsletterForm" action="php/newsletter-subscribe.php" method="POST">
-                                <div class="input-group">
-                                    <input class="form-control" placeholder="Correo Electrónico" name="newsletterEmail" id="newsletterEmail" type="text">
-                                    <span class="input-group-btn">
-                                        <button class="btn btn-default" type="enviar">Ir!</button>
-                                    </span>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="contact-details">
-                            <h4>Menú</h4>
-                            <ul class="contact">
-                                <a href="#"><li><p><strong>Inicio</strong></p></li></a>
-                                <a href="#"><li><p><strong>Nosotros</strong></p></li></a>
-                                <a href="#"><li><p><strong>Productos</strong></p></li></a>
-                                <a href="#"><li><p><strong>Mayoreo</strong></p></li></a>
-                                <a href="#"><li><p><strong>Sucursales</strong></p></li></a>
-                                <a href="#"><li><p><strong>Contáctanos</strong></p></li></a>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <h4>Siguenos</h4>
-                        <ul class="social-icons">
-                            <li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-                            <li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-2">
-                        <h4>Aceptamos</h4>
-                        <ul class="contact">
-                            <li><a href="#" target="_blank">{{ Html::image('template/img/visa.png') }}</i></a></li>
-                            <li><a href="#" target="_blank">{{ Html::image('template/img/mastercard.png') }}</i></a></li>
-                            <li><a href="#" target="_blank">{{ Html::image('template/img/paypal.png') }}</i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-copyright">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-7">
-                            <p>© {{ date('Y') }} MOBILEPHONE. Todos los derechos reservados.</p>
-                        </div>
-                        <div class="col-md-4">
-                            <nav id="sub-menu">
-                                <ul>
-                                    <li>Powered by</li>
-                                </ul>
-                            </nav>
-                        </div>
-                        <div class="col-md-1">
-                            <nav id="sub-menu">
-                                <a href="#" class="logo">
-                                    {{ Html::image('template/img/logo-footer.png') }}
-                                </a>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>               
-@endsection
-
-@section('js')
-<footer class="main-footer">
+      <footer class="main-footer">
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.3.7
     </div>
@@ -600,45 +519,46 @@
        immediately after the control sidebar -->
   <div class="control-sidebar-bg"></div>
 </div>
-<!-- ./wrapper -->
+<!-- ./wrapper -->          
+@endsection
 
-<!-- jQuery 2.2.3 -->
-<script src="plugins/jQuery/jquery-2.2.3.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button);
-</script>
-<!-- Bootstrap 3.3.6 -->
-<script src="bootstrap/js/bootstrap.min.js"></script>
-<!-- Morris.js charts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="plugins/morris/morris.min.js"></script>
-<!-- Sparkline -->
-<script src="plugins/sparkline/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
-<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="plugins/knob/jquery.knob.js"></script>
-<!-- daterangepicker -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-<script src="plugins/daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
-<script src="plugins/datepicker/bootstrap-datepicker.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
-<script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="plugins/fastclick/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/app.min.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
+@section('js')
+  <!-- jQuery 2.2.3 -->
+  {{ Html::script('contolpanel/plugins/jQuery/jquery-2.2.3.min.js') }}
+  <!-- jQuery UI 1.11.4 -->
+  <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+  <script>
+    $.widget.bridge('uibutton', $.ui.button);
+  </script>
+  <!-- Bootstrap 3.3.6 -->
+  {{ Html::script('controlpanel/bootstrap/js/bootstrap.min.js') }}
+  <!-- Morris.js charts -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+  {{ Html::script('controlpanel/plugins/morris/morris.min.js') }}
+  <!-- Sparkline -->
+  {{ Html::script('controlpanel/plugins/sparkline/jquery.sparkline.min.js') }}
+  <!-- jvectormap -->
+  {{ Html::script('controlpanel/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}
+  {{ Html::script('controlpanel/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}
+  <!-- jQuery Knob Chart -->
+  {{ Html::script('controlpanel/plugins/knob/jquery.knob.js') }}
+  <!-- daterangepicker -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
+  {{ Html::script('controlpanel/plugins/daterangepicker/daterangepicker.js') }}
+  <!-- datepicker -->
+  {{ Html::script('controlpanel/plugins/datepicker/bootstrap-datepicker.js') }}
+  <!-- Bootstrap WYSIHTML5 -->
+  {{ Html::script('controlpanel/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}
+  <!-- Slimscroll -->
+  {{ Html::script('controlpanel/plugins/slimScroll/jquery.slimscroll.min.js') }}
+  <!-- FastClick -->
+  {{ Html::script('controlpanel/plugins/fastclick/fastclick.js') }}
+  <!-- AdminLTE App -->
+  {{ Html::script('controlpanel/dist/js/app.min.js') }}
+  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+  {{ Html::script('controlpanel/dist/js/pages/dashboard.js') }}
+  <!-- AdminLTE for demo purposes -->
+  {{ Html::script('controlpanel/dist/js/demo.js') }}
 </body>
-</html>
 @endsection
